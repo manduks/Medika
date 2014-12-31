23c58889f9b625b8f6bbd2931ebb4f72e8e92fb1
Armando Gonzalez <iam@armando.mx>
2014-12-30 22:56:48 -0600

    added styles folder architecture folder

--- server/init.js
+++ server/init.js
@@ -1,6 +1,5 @@
 Meteor.startup(function() {
   return Inject.rawModHtml('addUnresolved', function(html) {
-    //return html = html.replace('<body>', '<body unresolved fit layout vertical>');
-    return html = html.replace('<body>', '<body fullbleed vertical layout unresolved>');
+    return html = html.replace('<body>', '<body unresolved>');
   });
 });
